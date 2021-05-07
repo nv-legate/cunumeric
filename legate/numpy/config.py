23af877796410ff45d139d6892e8cd04bfd490a0
Wonchan Lee <wonchanl@nvidia.com>
2021-05-07 00:57:08 -0700

    Minor fix

--- legate/numpy/config.py
+++ legate/numpy/config.py
@@ -194,21 +194,21 @@ class NumPyOpCode(IntEnum):
 
 @unique
 class BinaryOpCode(IntEnum):
-    ADD = (1,)
-    DIVIDE = (2,)
-    EQUAL = (3,)
-    FLOOR_DIVIDE = (4,)
-    GREATER = (5,)
-    GREATER_EQUAL = (6,)
-    LESS = (7,)
-    LESS_EQUAL = (8,)
-    MAXIMUM = (9,)
-    MINIUM = (10,)
-    MOD = (11,)
-    MULTIPLY = (12,)
-    NOT_EQUAL = (13,)
-    POWER = (14,)
-    SUBTRACT = (15,)
+    ADD = 1
+    DIVIDE = 2
+    EQUAL = 3
+    FLOOR_DIVIDE = 4
+    GREATER = 5
+    GREATER_EQUAL = 6
+    LESS = 7
+    LESS_EQUAL = 8
+    MAXIMUM = 9
+    MINIUM = 10
+    MOD = 11
+    MULTIPLY = 12
+    NOT_EQUAL = 13
+    POWER = 14
+    SUBTRACT = 15
 
 
 # Match these to NumPyRedopID in legate_numpy_c.h
