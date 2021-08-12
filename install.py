11bf5a239535c38e92496010838cb5386c276754
Manolis Papadakis <mpapadakis@nvidia.com>
2021-08-12 16:38:13 -0700

    Read default for openblas_dir like other options

--- install.py
+++ install.py
@@ -322,9 +322,7 @@ def install_legate_numpy(
         libs_config = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         libs_config = {}
-    if "OPEN_BLAS_DIR" in os.environ:
-        openblas_dir = os.environ["OPEN_BLAS_DIR"]
-    elif openblas_dir is None:
+    if openblas_dir is None:
         openblas_dir = libs_config.get("openblas")
         if openblas_dir is None:
             openblas_dir = os.path.join(legate_dir, "OpenBLAS")
@@ -420,6 +418,7 @@ def driver():
         dest="openblas_dir",
         metavar="DIR",
         required=False,
+        default=os.environ.get("OPEN_BLAS_DIR"),
         help="Path to OpenBLAS installation directory. Note that providing a "
         "user-defined BLAS library may lead to dynamic library conflicts with "
         "BLAS loaded by Python's Numpy. When using legate.numpy's BLAS, this "
