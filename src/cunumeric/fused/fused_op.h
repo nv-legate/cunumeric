7a58b6d87bacdb849a58765a48541e87df0da0e9
Shiv Sundram <shiv1@g0002.stanford.edu>
2021-12-01 17:06:15 -0800

    add missing header

---
+++ src/cunumeric/fused/fused_op.h
@@ -0,0 +1,46 @@
+/* Copyright 2021 NVIDIA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#pragma once
+
+#include "cunumeric/cunumeric.h"
+//#include "cunumeric/fused/binary_op_util.h"
+
+namespace cunumeric {
+/*
+struct BinaryOpArgs {
+  const Array& in1;
+  const Array& in2;
+  const Array& out;
+  BinaryOpCode op_code;
+  std::vector<legate::Store> args;
+};
+*/
+class FusedOpTask : public CuNumericTask<FusedOpTask> {
+ public:
+  static const int TASK_ID = CUNUMERIC_FUSED_OP;
+
+ public:
+  static void cpu_variant(legate::TaskContext& context);
+#ifdef LEGATE_USE_OPENMP
+  static void omp_variant(legate::TaskContext& context);
+#endif
+#ifdef LEGATE_USE_CUDA
+  static void gpu_variant(legate::TaskContext& context);
+#endif
+};
+
+}  // namespace cunumeric
