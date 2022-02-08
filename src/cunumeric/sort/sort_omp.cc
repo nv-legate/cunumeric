85bc3a73c5ca59adf74a2c939724daadb70bcdd6
mfoerste4 <mfoerster@nvidia.com>
2022-02-08 21:48:03 +0100

    switch to parallel gcc sort

--- src/cunumeric/sort/sort_omp.cc
+++ src/cunumeric/sort/sort_omp.cc
@@ -17,57 +17,13 @@
 #include "cunumeric/sort/sort.h"
 #include "cunumeric/sort/sort_template.inl"
 
+#include <parallel/algorithm>
 #include <omp.h>
 
 namespace cunumeric {
 
 using namespace Legion;
 using namespace legate;
-
-template <typename VAL>
-void merge(VAL* inptr, size_t start_idx, size_t end_idx, VAL* tmp)
-{
-  const size_t mid  = (end_idx + start_idx) / 2;
-  size_t left_idx   = start_idx;
-  size_t right_idx  = mid;
-  size_t target_idx = start_idx;
-
-  while (left_idx < mid && right_idx < end_idx) {
-    if (inptr[left_idx] <= inptr[right_idx]) {
-      tmp[target_idx++] = inptr[left_idx++];
-    } else {
-      tmp[target_idx++] = inptr[right_idx++];
-    }
-  }
-
-  while (left_idx < mid) { tmp[target_idx++] = inptr[left_idx++]; }
-  while (right_idx < end_idx) { tmp[target_idx++] = inptr[right_idx++]; }
-
-  std::copy(tmp + start_idx, tmp + end_idx, inptr + start_idx);
-}
-
-// TODO tune
-#define SEQUENTIAL_THRESHOLD 1024
-#define TASK_THRESHOLD 2048
-
-template <typename VAL>
-void merge_sort(VAL* inptr, const size_t start_idx, const size_t end_idx, VAL* tmp)
-{
-  const size_t size = end_idx - start_idx + 1;
-  if (size > SEQUENTIAL_THRESHOLD) {
-    const size_t mid = (end_idx + start_idx) / 2;
-
-#pragma omp task shared(inptr, tmp) if (size > TASK_THRESHOLD)
-    merge_sort(inptr, start_idx, mid, tmp);
-#pragma omp task shared(inptr, tmp) if (size > TASK_THRESHOLD)
-    merge_sort(inptr, mid, end_idx, tmp);
-
-#pragma omp taskwait
-    merge(inptr, start_idx, end_idx, tmp);
-  } else if (size > 1) {
-    std::stable_sort(inptr + start_idx, inptr + end_idx);
-  }
-}
 
 template <LegateTypeCode CODE, int32_t DIM>
 struct SortImplBody<VariantKind::OMP, CODE, DIM> {
@@ -88,24 +44,12 @@ struct SortImplBody<VariantKind::OMP, CODE, DIM> {
               << domain.get_volume() << std::endl;
 #endif
 
-    bool nested = omp_get_nested();
-    if (!nested) omp_set_nested(1);
-
-    // merge sort
-    auto tmp = std::make_unique<VAL[]>(volume);
-
-#pragma omp parallel shared(inptr, tmp)
-    {
-#pragma omp single
-      merge_sort(inptr, 0, volume, &(tmp[0]));
-    }
+    __gnu_parallel::stable_sort(inptr, inptr + volume);
 
     if (is_index_space) {
       // not implemented yet
       assert(false);
     }
-
-    if (!nested) omp_set_nested(0);
   }
 };
 
