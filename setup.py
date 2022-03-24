dad4248d8c94546b16f40e8564fe6acdb88f7960
Bryan Van de Ven <bvandeven@nvidia.com>
2022-03-24 10:27:34 -0700

    add cunumeric.ufunc to packages

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ if args.recurse:
             "cunumeric",
             "cunumeric.linalg",
             "cunumeric.random",
+            "cunumeric.ufunc",
         ],
         cmdclass={"build_py": my_build_py},
     )
