6f98ccbfc143b5cf4144ccb381828657c62e2940
Irina Demeshko <idemeshko@nvidia.com>
2022-04-26 21:35:51 -0600

    implementing TRACE operation (#263)
     implementing TRACE operation

--- cunumeric/thunk.py
+++ cunumeric/thunk.py
@@ -143,13 +143,7 @@ class NumPyThunk(ABC):
         ...
 
     @abstractmethod
-    def _diag_helper(
-        self,
-        rhs,
-        offset,
-        naxes,
-        extract,
-    ):
+    def _diag_helper(self, rhs, offset, naxes, extract, trace):
         ...
 
     @abstractmethod
