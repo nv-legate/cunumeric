3cc1b57d367fb88513380c183a0fe43513e83aeb
Wonchan Lee <wonchanl@nvidia.com>
2022-05-10 16:01:42 -0700

    Revise tests for testing NumPy's ndarrays as outputs

--- tests/integration/test_unary_ufunc.py
+++ tests/integration/test_unary_ufunc.py
@@ -68,11 +68,8 @@ def check_ops(ops, in_np, out_dtype="d"):
 
         check_result(op, in_np, out_np, out_num)
 
-        out_np = np.empty(out_np.shape, dtype=out_dtype)
         # Ask cuNumeric to produce outputs to NumPy ndarrays
-        out_num = np.empty(out_num.shape, dtype=out_dtype)
-
-        op_np(*in_np, out_np)
+        out_num = np.ones(out_np.shape, dtype=out_dtype)
         op_num(*in_num, out_num)
 
         check_result(op, in_np, out_np, out_num)
