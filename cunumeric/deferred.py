fd4a210e810840435796b84a9f07050ce6dc2788
Irina Demeshko <idemeshko@nvidia.com>
2022-05-13 15:53:43 -0600

    separating logic for creating output store for the case of Future store and non-Future store

--- cunumeric/deferred.py
+++ cunumeric/deferred.py
@@ -632,19 +632,24 @@ class DeferredArray(NumPyThunk):
             ) = self._create_indexing_array(key)
             store = rhs.base
             if copy_needed:
-                # Create a new array to be the result
-                result_store = self.context.create_store(
-                    self.dtype,
-                    shape=index_array.shape,
-                    optimize_scalar=False,
-                )
-                result = DeferredArray(
-                    self.runtime,
-                    base=result_store,
-                    dtype=self.dtype,
-                )
                 if index_array.base.kind == Future:
                     index_array = self._convert_future_to_store(index_array)
+                    result_store = self.context.create_store(
+                        self.dtype,
+                        shape=index_array.shape,
+                        optimize_scalar=False,
+                    )
+                    result = DeferredArray(
+                        self.runtime,
+                        base=result_store,
+                        dtype=self.dtype,
+                    )
+                else:
+                    result = self.runtime.create_empty_thunk(
+                        index_array.base.shape,
+                        self.dtype,
+                        inputs=[self],
+                    )
 
                 copy = self.context.create_copy()
                 copy.set_source_indirect_out_of_range(False)
