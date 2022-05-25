ac77bd955633d72af47196f732aad20a4edabfe1
robinw0928 <104830875+robinw0928@users.noreply.github.com>
2022-05-26 02:18:58 +0800

    Add support for using cupy in some examples (#373)
    Add support for using cupy
    
    1. Add option to specify using legate, cupy or numpy
    2. Add option to specify which memory pool to use for cupy
    3. Set copy=False for astype in logreg.
    4. Add option to select how einsum function is called as cupy only
    supports C = np.einsum(expr, A, B)
    5. Define time methods for cupy
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- examples/jacobi.py
+++ examples/jacobi.py
@@ -19,9 +19,14 @@ import argparse
 import math
 
 from benchmark import run_benchmark
-from legate.timing import time
 
-import cunumeric as np
+try:
+    from legate.timing import time
+except ImportError:
+    from time import perf_counter_ns
+
+    def time():
+        return perf_counter_ns() / 1000.0
 
 
 def generate_random(N):
@@ -117,8 +122,41 @@ if __name__ == "__main__":
         help="number of times to benchmark this application (default 1 - "
         "normal execution)",
     )
+    parser.add_argument(
+        "--package",
+        dest="package",
+        choices=["legate", "numpy", "cupy"],
+        type=str,
+        default="legate",
+        help="NumPy package to use (legate, numpy, or cupy)",
+    )
+    parser.add_argument(
+        "--cupy-allocator",
+        dest="cupy_allocator",
+        choices=["default", "off", "managed"],
+        type=str,
+        default="default",
+        help="cupy allocator to use (default, off, or managed)",
+    )
 
     args = parser.parse_args()
+
+    if args.package == "legate":
+        import cunumeric as np
+    elif args.package == "cupy":
+        import cupy as np
+
+        if args.cupy_allocator == "off":
+            np.cuda.set_allocator(None)
+            print("Turning off memory pool")
+        elif args.cupy_allocator == "managed":
+            np.cuda.set_allocator(
+                np.cuda.MemoryPool(np.cuda.malloc_managed).malloc
+            )
+            print("Using managed memory pool")
+    elif args.package == "numpy":
+        import numpy as np
+
     run_benchmark(
         run_jacobi,
         args.benchmark,
