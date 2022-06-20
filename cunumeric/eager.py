b90ea94273a23f65d83f6f7123e0587a751b120f
Malte Foerster <mfoerster@nvidia.com>
2022-06-20 19:54:13 +0000

    fixed eager parameter

--- cunumeric/eager.py
+++ cunumeric/eager.py
@@ -613,7 +613,7 @@ class EagerArray(NumPyThunk):
         if self.deferred is not None:
             self.deferred.searchsorted(rhs, v, side)
         else:
-            self.array = np.searchsorted(rhs.array, v.array)
+            self.array = np.searchsorted(rhs.array, v.array, side=side)
 
     def sort(self, rhs, argsort=False, axis=-1, kind="quicksort", order=None):
         self.check_eager_args(rhs, axis, kind, order)
