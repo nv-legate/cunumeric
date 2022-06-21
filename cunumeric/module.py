052023e506e17dcc11234263cd998a58bd986fe3
Irina Demeshko <idemeshko@nvidia.com>
2022-06-21 02:37:57 -0600

    adding support for tri, tril_indices, trilu_indices, tril_indices_from, trilu_indices_from

--- cunumeric/module.py
+++ cunumeric/module.py
@@ -797,6 +797,60 @@ def diag(v, k=0):
         raise ValueError("diag requires 1- or 2-D array, use diagonal instead")
 
 
+def tri(N, M=None, k=0, dtype=float, *, like=None):
+    """
+    An array with ones at and below the given diagonal and zeros elsewhere.
+
+    Parameters
+    ----------
+    N : int
+        Number of rows in the array.
+    M : int, optional
+        Number of columns in the array.
+        By default, `M` is taken equal to `N`.
+    k : int, optional
+        The sub-diagonal at and below which the array is filled.
+        `k` = 0 is the main diagonal, while `k` < 0 is below it,
+        and `k` > 0 is above.  The default is 0.
+    dtype : dtype, optional
+        Data type of the returned array.  The default is float.
+    like : array_like
+        Reference object to allow the creation of arrays which are not NumPy
+        arrays. If an array-like passed in as `like` supports the
+        `__array_function__ protocol, the result will be defined by it. In this
+        case it ensures the creation of an array object compatible with that
+        passed in via this argument.
+
+    Returns
+    -------
+    tri : ndarray of shape (N, M)
+        Array with its lower triangle filled with ones and zero elsewhere;
+        in other words ``T[i,j] == 1`` for ``j <= i + k``, 0 otherwise.
+
+    See Also
+    --------
+    numpy.tri
+
+    Notes
+    -----
+    `like` argument is currently not supported
+
+    Availability
+    --------
+    Multiple GPUs, Multiple CPUs
+
+    """
+    # TODO: add support for `like`
+    if like is not None:
+        raise ValueError("like parameter is currently not supported")
+
+    if M is None:
+        M = N
+
+    out = ones((N, M), dtype=dtype)
+    return tril(out, k)
+
+
 @add_boilerplate("m")
 def trilu(m, k, lower):
     if m.ndim < 1:
@@ -2370,6 +2424,152 @@ def diag_indices_from(arr):
             raise ValueError("All dimensions of input must be of equal length")
 
     return diag_indices(arr.shape[0], arr.ndim)
+
+
+def tril_indices(n, k=0, m=None):
+    """
+    Return the indices for the lower-triangle of an (n, m) array.
+
+    Parameters
+    ----------
+    n : int
+        The row dimension of the arrays for which the returned
+        indices will be valid.
+    k : int, optional
+        Diagonal offset (see `tril` for details).
+    m : int, optional
+        The column dimension of the arrays for which the returned
+        arrays will be valid.
+        By default `m` is taken equal to `n`.
+
+    Returns
+    -------
+    inds : tuple of arrays
+        The indices for the triangle. The returned tuple contains two arrays,
+        each with the indices along one dimension of the array.
+
+    See also
+    --------
+    numpy.tril_indices
+
+    Notes
+    -----
+
+    Availability
+    ------------
+    Multiple GPUs, Multiple CPUs
+    """
+
+    tri_ = tri(n, m, k=k, dtype=bool)
+    return nonzero(tri_)
+
+
+@add_boilerplate("arr")
+def tril_indices_from(arr, k=0):
+    """
+    Return the indices for the lower-triangle of arr.
+
+    See `tril_indices` for full details.
+
+    Parameters
+    ----------
+    arr : array_like
+        The indices will be valid for square arrays whose dimensions are
+        the same as arr.
+    k : int, optional
+        Diagonal offset (see `tril` for details).
+
+    See Also
+    --------
+    numpy.tril_indices_from
+
+    Notes
+    -----
+
+    Availability
+    --------
+    Multiple GPUs, Multiple CPUs
+
+    """
+    # this implementation is taken from numpy
+    if arr.ndim != 2:
+        raise ValueError("input array must be 2-d")
+    return tril_indices(arr.shape[-2], k=k, m=arr.shape[-1])
+
+
+def triu_indices(n, k=0, m=None):
+    """
+    Return the indices for the upper-triangle of an (n, m) array.
+
+    Parameters
+    ----------
+    n : int
+        The size of the arrays for which the returned indices will
+        be valid.
+    k : int, optional
+        Diagonal offset (see `triu` for details).
+    m : int, optional
+        The column dimension of the arrays for which the returned
+        arrays will be valid.
+        By default `m` is taken equal to `n`.
+
+    Returns
+    -------
+    inds : tuple, shape(2) of ndarrays, shape(`n`)
+        The indices for the triangle. The returned tuple contains two arrays,
+        each with the indices along one dimension of the array.  Can be used
+        to slice a ndarray of shape(`n`, `n`).
+
+    See also
+    --------
+    numpy.triu_indices
+
+    Notes
+    -----
+
+    Availability
+    --------
+    Multiple GPUs, Multiple CPUs
+    """
+
+    tri_ = ~tri(n, m, k=k - 1, dtype=bool)
+    return nonzero(tri_)
+
+
+@add_boilerplate("arr")
+def triu_indices_from(arr, k=0):
+    """
+    Return the indices for the upper-triangle of arr.
+
+    See `triu_indices` for full details.
+
+    Parameters
+    ----------
+    arr : ndarray, shape(N, N)
+        The indices will be valid for square arrays.
+    k : int, optional
+        Diagonal offset (see `triu` for details).
+
+    Returns
+    -------
+    triu_indices_from : tuple, shape(2) of ndarray, shape(N)
+        Indices for the upper-triangle of `arr`.
+
+    See Also
+    --------
+    numpy.triu_indices_from
+
+    Notes
+    -----
+
+    Availability
+    --------
+    Multiple GPUs, Multiple CPUs
+    """
+    # this implementation is taken from numpy
+    if arr.ndim != 2:
+        raise ValueError("input array must be 2-d")
+    return triu_indices(arr.shape[-2], k=k, m=arr.shape[-1])
 
 
 @add_boilerplate("a")
