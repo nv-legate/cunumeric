1d5acf29af8bd4e128bd0f815c0fb719f8c6c343
Irina Demeshko <idemeshko@nvidia.com>
2022-06-29 13:20:41 -0600

    fixing tests

--- tests/integration/test_take_along_axis.py
+++ tests/integration/test_take_along_axis.py
@@ -14,13 +14,14 @@
 #
 
 import numpy as np
+import pytest
 from test_tools.generators import mk_seq_array
 
 import cunumeric as num
 from legate.core import LEGATE_MAX_DIM
 
 
-def test():
+def test_3d():
 
     x = mk_seq_array(np, (256, 256, 100))
     x_num = mk_seq_array(num, (256, 256, 100))
@@ -32,6 +33,11 @@ def test():
     res_num = num.take_along_axis(x_num, indices_num, -1)
     assert np.array_equal(res_num, res)
 
+
+def test_None_axis():
+    x = mk_seq_array(np, (256, 256, 100))
+    x_num = mk_seq_array(num, (256, 256, 100))
+
     # testig the case when axis = None
     indices = mk_seq_array(np, (256,))
     indices_num = num.array(indices)
@@ -40,24 +46,26 @@ def test():
     res_num = num.take_along_axis(x_num, indices_num, None)
     assert np.array_equal(res_num, res)
 
-    for ndim in range(1, LEGATE_MAX_DIM + 1):
-        shape = (5,) * ndim
-        np_arr = mk_seq_array(np, shape)
-        num_arr = mk_seq_array(num, shape)
-        shape_idx = (1,) * ndim
-        np_indices = mk_seq_array(np, shape_idx) % 5
-        num_indices = mk_seq_array(num, shape_idx) % 5
-        for axis in range(ndim):
-            res_np = np.take_along_axis(np_arr, np_indices, axis=axis)
-            res_num = num.take_along_axis(num_arr, num_indices, axis=axis)
-            assert np.array_equal(res_num, res_np)
-        np_indices = mk_seq_array(np, (3,))
-        num_indices = mk_seq_array(num, (3,))
-        res_np = np.take_along_axis(np_arr, np_indices, None)
-        res_num = num.take_along_axis(num_arr, num_indices, None)
 
-    return
+@pytest.mark.parametrize("ndim", range(1, LEGATE_MAX_DIM + 1))
+def test_ndim(ndim):
+    shape = (5,) * ndim
+    np_arr = mk_seq_array(np, shape)
+    num_arr = mk_seq_array(num, shape)
+    shape_idx = (1,) * ndim
+    np_indices = mk_seq_array(np, shape_idx) % 5
+    num_indices = mk_seq_array(num, shape_idx) % 5
+    for axis in range(ndim):
+        res_np = np.take_along_axis(np_arr, np_indices, axis=axis)
+        res_num = num.take_along_axis(num_arr, num_indices, axis=axis)
+        assert np.array_equal(res_num, res_np)
+    np_indices = mk_seq_array(np, (3,))
+    num_indices = mk_seq_array(num, (3,))
+    res_np = np.take_along_axis(np_arr, np_indices, None)
+    res_num = num.take_along_axis(num_arr, num_indices, None)
 
 
 if __name__ == "__main__":
-    test()
+    import sys
+
+    sys.exit(pytest.main(sys.argv))
