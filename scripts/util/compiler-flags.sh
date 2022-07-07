09ad0fc1e401eefda8cd3de20ddab21cead2e1f9
ptaylor <paul.e.taylor@me.com>
2022-07-06 22:20:45 -0700

    add scikit-build

---
+++ scripts/util/compiler-flags.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+export CFLAGS="-w -fdiagnostics-color=always"
+export CXXFLAGS="-w -fdiagnostics-color=always"
+export CUDAFLAGS="-w -Xcompiler=-w,-fdiagnostics-color=always"
