9914463f7007ba8cec9f89611115ee3960f439a6
ptaylor <paul.e.taylor@me.com>
2022-07-07 09:33:40 -0700

    export LIBRARY_PATH if not set

--- scripts/util/compiler-flags.sh
+++ scripts/util/compiler-flags.sh
@@ -3,3 +3,9 @@
 export CFLAGS="-w -fdiagnostics-color=always"
 export CXXFLAGS="-w -fdiagnostics-color=always"
 export CUDAFLAGS="-w -Xcompiler=-w,-fdiagnostics-color=always"
+
+if [[ -z "$LIBRARY_PATH" ]]; then
+    if [[ -d "${CUDA_HOME:-/usr/local/cuda}/lib64/stubs" ]]; then
+        export LIBRARY_PATH="${CUDA_HOME:-/usr/local/cuda}/lib64/stubs"
+    fi
+fi
