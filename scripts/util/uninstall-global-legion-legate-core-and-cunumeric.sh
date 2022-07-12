02b3e36d395c90db86beabfbbef89b1d73b9e1cb
ptaylor <paul.e.taylor@me.com>
2022-07-12 16:29:26 -0700

    clean out tblis lib and include dirs

--- scripts/util/uninstall-global-legion-legate-core-and-cunumeric.sh
+++ scripts/util/uninstall-global-legion-legate-core-and-cunumeric.sh
@@ -2,5 +2,8 @@
 
 rm -rf $(find "$CONDA_PREFIX/lib" -type d -name '*cunumeric*') \
        $(find "$CONDA_PREFIX/lib" -type f -name 'libcunumeric*') \
-       $(find "$CONDA_PREFIX/include" -type d -name '*cunumeric*') \
+       $(find "$CONDA_PREFIX/include" -type f -name 'tci.h') \
+       $(find "$CONDA_PREFIX/include" -type d -name 'tci') \
+       $(find "$CONDA_PREFIX/include" -type d -name 'tblis') \
+       $(find "$CONDA_PREFIX/include" -type d -name 'cunumeric') \
        ;
