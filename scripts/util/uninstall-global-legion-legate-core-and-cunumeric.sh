d43f7652f02d39f4dee0c05af37860613f1717a3
ptaylor <paul.e.taylor@me.com>
2022-08-25 10:13:53 -0700

    don't remove easy-install.pth

--- scripts/util/uninstall-global-legion-legate-core-and-cunumeric.sh
+++ scripts/util/uninstall-global-legion-legate-core-and-cunumeric.sh
@@ -2,7 +2,6 @@
 
 rm -rf $(find "$CONDA_PREFIX/lib" -type d -name '*cunumeric*') \
        $(find "$CONDA_PREFIX/lib" -type f -name 'libcunumeric*') \
-       $(find "$CONDA_PREFIX/lib" -type f -name 'easy-install.pth') \
        $(find "$CONDA_PREFIX/lib" -type f -name 'cunumeric.egg-link') \
        $(find "$CONDA_PREFIX/include" -type f -name 'tci.h') \
        $(find "$CONDA_PREFIX/include" -type d -name 'tci') \
