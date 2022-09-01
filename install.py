a8d31a85a7e4e026c7f488093febfb1a69114483
Marcin Zalewski <mzalewski@nvidia.com>
2022-09-01 11:50:17 -0700

    Rename thread count env var to match conda build

--- install.py
+++ install.py
@@ -619,7 +619,7 @@ def driver():
         nargs="?",
         type=int,
         required=False,
-        default=os.environ.get("THREAD_COUNT"),
+        default=os.environ.get("CPU_COUNT"),
         help="Number of threads used to compile.",
     )
     parser.add_argument(
