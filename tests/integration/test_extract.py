78a08863a252dcf0ba7bfb50e7954b340cedac76
xialu00 <xial@nvidia.com>
2022-10-28 00:37:16 +0800

    add test case for test_extract.py

--- tests/integration/test_extract.py
+++ tests/integration/test_extract.py
@@ -15,99 +15,102 @@
 
 import numpy as np
 import pytest
+from utils.generators import mk_seq_array
 
 import cunumeric as num
 
-np.random.seed(42)
-
-
-def test_extract():
-    cnp = np.array(
-        [1, 54, 4, 4, 0, 45, 5, 58, 0, 9, 0, 4, 0, 0, 0, 5, 0, 1]
-    ).reshape(
-        (6, 3)
-    )  # noqa E501
-    c = num.array(cnp)
-    bnp = np.random.randn(6, 3)
-    b = num.array(bnp)
-    assert num.array_equal(num.extract(c, b), np.extract(cnp, bnp))
-
-
-ARR = [
-    [1, 54, 4, 4, 0, 45, 5, 58, 0, 9, 0, 4, 0, 0, 0, 5, 0, 1],
-    [[1, 54, 4], [4, 0, 45], [5, 58, 0], [9, 0, 4], [0, 0, 0], [5, 0, 1]],
-    [
-        [[1, 54, 4], [4, 0, 45]],
-        [[5, 58, 0], [9, 0, 4]],
-        [[0, 0, 0], [5, 0, 1]],
-    ],
-    [[[1 + 2j, 54, 4], [4, 0 + 1j, 45]], [[5, 58, 0], [9, 0, 4]]],
-    [[True, False], [True, True], [True, False]],
-    [[]],
-    [],
-    [
-        [[0, 0, 0], [0, 0, 0]],
-        [[0, 0, 0], [0, 0, 1]],
-    ],
-    [False, False, False],
-    [
-        [[0, 0, 0], [0, 0, 0]],
-        [[0, 0, 0], [0, 0, 0]],
-    ],
+DIM = 5
+SIZES = [
+    (0,),
+    1,
+    5,
+    (0, 1),
+    (1, 0),
+    (1, 1),
+    (1, DIM),
+    (DIM, 1),
+    (DIM, DIM),
+    (1, 0, 0),
+    (1, 1, 0),
+    (1, 0, 1),
+    (1, 1, 1),
+    (DIM, 1, 1),
+    (1, DIM, 1),
+    (1, 1, DIM),
+    (DIM, DIM, DIM),
 ]
-
-
-def check_extract(condition_np, arr_np):
-    arr_num = num.array(arr_np)
-    condition_num = num.array(condition_np)
-    result_np = np.extract(condition_np, arr_np)
-    result_np2 = arr_np[condition_np.reshape(arr_np.shape).astype(bool)]
-    assert np.array_equal(result_np, result_np2)
-    result_num = num.extract(condition_num, arr_num)
-    assert np.array_equal(result_np, result_num)
-
-
-@pytest.mark.parametrize("arr", ARR, ids=str)
-def test_extract_bool(arr):
-    arr_np = np.array(arr)
-    condition_np = arr_np != 0
-    check_extract(condition_np, arr_np)
-    check_extract(condition_np.flatten(), arr_np)
-    check_extract(condition_np, arr_np.flatten())
-    check_extract(condition_np.swapaxes(0, condition_np.ndim - 1), arr_np)
-    check_extract(condition_np, arr_np.swapaxes(0, condition_np.ndim - 1))
-
-
-@pytest.mark.parametrize("arr", ARR, ids=str)
-def test_extract_nonzero(arr):
-    arr_np = np.array(arr)
-    condition_np = arr_np.copy()
-    check_extract(condition_np, arr_np)
-    check_extract(condition_np.flatten(), arr_np)
-    check_extract(condition_np, arr_np.flatten())
-    check_extract(condition_np.swapaxes(0, condition_np.ndim - 1), arr_np)
-    check_extract(condition_np, arr_np.swapaxes(0, condition_np.ndim - 1))
-
 
 VALUES = [
-    [11, 12, 13],
-    [99, 93, 76, 65, 76, 87, 43, 23, 12, 54, 756, 2345, 232, 2323, 12145],
+    [0],
     [42],
-    [True, False, False, True],
-    [42.3, 42.3, 42.3, 42.3, 42.3, 42.3, 42.3, 42.3],
     [42 + 3j],
+    [11, 12, 13],
+    [True, False, False, True],
+    [42.3, 42.3, 42.3, 42.3, 42.3],
+    [np.inf, np.Inf],
 ]
 
 
-@pytest.mark.parametrize("arr", ARR, ids=str)
-@pytest.mark.parametrize("vals", VALUES, ids=str)
-def test_place(arr, vals):
-    arr_np = np.array(arr)
-    vals_np = np.array(vals).astype(arr_np.dtype)
-    condition_np = arr_np != 0
+@pytest.mark.xfail
+def test_none_array():
+    res_np = np.extract([0], None)  # return []
+    res_num = num.extract(
+        [0], None
+    )  # AttributeError: 'NoneType' object has no attribute 'size'
+    assert np.array_equal(res_np, res_num)
 
-    arr_num = num.array(arr_np)
-    condition_num = num.array(condition_np)
+
+@pytest.mark.xfail
+def test_empty_array():
+    res_np = np.extract([0], [])  # return []
+    res_num = num.extract(
+        [0], []
+    )  # ValueError: arr array and condition array must be of same size
+    assert np.array_equal(res_np, res_num)
+
+
+@pytest.mark.xfail
+def test_none_condition():
+    a = num.array([1, 2, 3, 4])
+    res_np = np.extract(None, a)  # all return []
+    res_num = num.extract(
+        None, a
+    )  # AttributeError: 'NoneType' object has no attribute 'size'
+    assert np.array_equal(res_np, res_num)
+
+
+@pytest.mark.parametrize(
+    "con", (-3, 0, 3, False, True, [2], [2, 3], [2, -3, 4], [1, 2, 3, 4, 5])
+)
+def test_negative_condition(con):
+    a = num.array([1, 2, 3, 4])
+    with pytest.raises(ValueError):
+        num.extract(con, a)
+
+
+@pytest.mark.parametrize("shape", SIZES, ids=str)
+def test_extract_basic(shape):
+    np_arr = mk_seq_array(np, shape)
+    num_arr = mk_seq_array(num, shape)
+    # make sure condition is between 1 and 2
+    np_condition = mk_seq_array(np, shape) % 2
+    num_condition = mk_seq_array(num, shape) % 2
+
+    res_np = np.extract(np_condition, np_arr)
+    res_num = num.extract(num_condition, num_arr)
+    assert np.array_equal(res_num, res_np)
+
+
+@pytest.mark.parametrize("shape", SIZES, ids=str)
+@pytest.mark.parametrize("vals", VALUES, ids=str)
+def test_place_basic(shape, vals):
+    arr_np = mk_seq_array(np, shape)
+    arr_num = num.array(mk_seq_array(num, shape))
+
+    condition_np = mk_seq_array(np, shape) % 3
+    condition_num = mk_seq_array(num, shape) % 3
+
+    vals_np = np.array(vals).astype(arr_np.dtype)
     vals_num = num.array(vals_np)
 
     np.place(arr_np, condition_np, vals_np)
