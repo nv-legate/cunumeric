5d3f743ba21e6458d7ee611935ec952314edad77
Paul Taylor <paul.e.taylor@me.com>
2022-11-04 13:18:35 -0700

    Make `install.py` reconfigure editable installs when build type changes (#670)
    * pass -mindepth 1 so we don't accidentally delete the search root if it matches one of the `-d` names
    
    * pass unknown flags to `pip install` command
    
    * use CMAKE_ARGS instead of SKBUILD_CONFIGURE_OPTIONS to work around scikit-build bug
    
    * replace SKBUILD_CONFIGURE_OPTIONS with CMAKE_ARGS everywhere

--- scripts/util/uninstall-global-legion-legate-core-and-cunumeric.sh
+++ scripts/util/uninstall-global-legion-legate-core-and-cunumeric.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-rm -rf $(find "$CONDA_PREFIX/lib" -type d -name '*cunumeric*') \
-       $(find "$CONDA_PREFIX/lib" -type f -name 'libcunumeric*') \
-       $(find "$CONDA_PREFIX/lib" -type f -name 'cunumeric.egg-link') \
-       $(find "$CONDA_PREFIX/include" -type f -name 'tci.h') \
-       $(find "$CONDA_PREFIX/include" -type d -name 'tci') \
-       $(find "$CONDA_PREFIX/include" -type d -name 'tblis') \
-       $(find "$CONDA_PREFIX/include" -type d -name 'cunumeric') \
+rm -rf $(find "$CONDA_PREFIX/lib" -mindepth 1 -type d -name '*cunumeric*') \
+       $(find "$CONDA_PREFIX/lib" -mindepth 1 -type f -name 'libcunumeric*') \
+       $(find "$CONDA_PREFIX/lib" -mindepth 1 -type f -name 'cunumeric.egg-link') \
+       $(find "$CONDA_PREFIX/include" -mindepth 1 -type f -name 'tci.h') \
+       $(find "$CONDA_PREFIX/include" -mindepth 1 -type d -name 'tci') \
+       $(find "$CONDA_PREFIX/include" -mindepth 1 -type d -name 'tblis') \
+       $(find "$CONDA_PREFIX/include" -mindepth 1 -type d -name 'cunumeric') \
        ;
