6b835d6df4d693937344688fb33cecb46ef41e5d
robinw0928 <104830875+robinw0928@users.noreply.github.com>
2022-11-11 11:05:11 +0800

    Enhance test_linspace.py and test_swapaxes.py (#680)
    * Enhance test_linspace.py and test_swapaxes.py
    
    * Address comments.
    
    * Address comments part-2.

--- tests/integration/test_linspace.py
+++ tests/integration/test_linspace.py
@@ -13,48 +13,315 @@
 # limitations under the License.
 #
 
+from itertools import chain
+
 import numpy as np
 import pytest
+from utils.generators import broadcasts_to, mk_seq_array
 
 import cunumeric as num
 
 
-def test_basic():
-    x = np.linspace(2.0, 3.0, num=5)
-    y = num.linspace(2.0, 3.0, num=5)
+def equivalent_shapes_gen(shape):
+    """
+    Generate more equivalent shapes by removing
+    leading singleton dimensions from `shape`.
+    e.g., shape=(1, 4, 1), yield (1, 4, 1), (4, 1)
+    shape=(1, 1, 5), yield (1, 1, 5), (1, 5), (5,)
+    """
+    yield shape
+    for i in range(len(shape) - 1):
+        if shape[i] == 1:
+            i += 1
+            yield shape[i:]
+        else:
+            break
+
+
+@pytest.mark.parametrize(
+    "endpoint", (True, False), ids=lambda endpoint: f"(endpoint={endpoint})"
+)
+@pytest.mark.parametrize(
+    "number", (0, 1, 10), ids=lambda number: f"(num={number})"
+)
+@pytest.mark.parametrize(
+    "values",
+    ((10, -5.5), (2.0, 3.0), (0, 0), (1 + 2.5j, 10 + 5j), (0j, 10)),
+    ids=lambda values: f"(values={values})",
+)
+def test_scalar_basic(values, number, endpoint):
+    start, stop = values
+    x = np.linspace(start, stop, num=number, endpoint=endpoint)
+    y = num.linspace(start, stop, num=number, endpoint=endpoint)
     assert np.array_equal(x, y)
 
 
-def test_endpoint():
-    x = np.linspace(2.0, 3.0, num=5, endpoint=False)
-    y = num.linspace(2.0, 3.0, num=5, endpoint=False)
-    assert np.array_equal(x, y)
+@pytest.mark.parametrize(
+    "endpoint", (True, False), ids=lambda endpoint: f"(endpoint={endpoint})"
+)
+@pytest.mark.parametrize(
+    "number", (0, 1, 10), ids=lambda number: f"(num={number})"
+)
+@pytest.mark.parametrize(
+    "values",
+    ((10, -5.5), (2.0, 3.0), (0, 0), (1 + 2.5j, 10 + 5j), (0j, 10)),
+    ids=lambda values: f"(values={values})",
+)
+def test_scalar_basic_retstep(values, number, endpoint):
+    start, stop = values
+    x = np.linspace(start, stop, num=number, endpoint=endpoint, retstep=True)
+    y = num.linspace(start, stop, num=number, endpoint=endpoint, retstep=True)
 
-
-def test_retstep():
-    x = np.linspace(2.0, 3.0, num=5, retstep=True)
-    y = np.linspace(2.0, 3.0, num=5, retstep=True)
     assert np.array_equal(x[0], y[0])
-    assert x[1] == y[1]
+    if not (np.isnan(x[1]) and np.isnan(y[1])):
+        assert x[1] == y[1]
 
 
-def test_axis():
+@pytest.mark.parametrize(
+    "endpoint", (True, False), ids=lambda endpoint: f"(endpoint={endpoint})"
+)
+def test_arrays_basic(endpoint):
+    shape = (2, 2, 3)
+    np_start = mk_seq_array(np, shape)
+    num_start = mk_seq_array(num, shape)
+    np_stop = mk_seq_array(np, shape) + 10
+    num_stop = mk_seq_array(num, shape) + 10
+    x = np.linspace(np_start, np_stop, num=5, endpoint=endpoint)
+    y = np.linspace(num_start, num_stop, num=5, endpoint=endpoint)
+    assert np.array_equal(x, y)
+
+
+@pytest.mark.parametrize(
+    "endpoint", (True, False), ids=lambda endpoint: f"(endpoint={endpoint})"
+)
+def test_arrays_basic_retstep(endpoint):
+    shape = (2, 2, 3)
+    np_start = mk_seq_array(np, shape)
+    num_start = mk_seq_array(num, shape)
+    np_stop = mk_seq_array(np, shape) + 10
+    num_stop = mk_seq_array(num, shape) + 10
+    x = np.linspace(np_start, np_stop, num=5, endpoint=endpoint, retstep=True)
+    y = np.linspace(
+        num_start, num_stop, num=5, endpoint=endpoint, retstep=True
+    )
+    assert np.array_equal(x[0], y[0])
+    assert np.array_equal(x[1], y[1])
+
+
+shape_start = (2, 2, 3)
+shape_stops = (equivalent_shapes_gen(s) for s in broadcasts_to(shape_start))
+
+
+@pytest.mark.parametrize(
+    "shape_stop",
+    chain.from_iterable(shape_stops),
+    ids=lambda shape_stop: f"(shape_stop={shape_stop})",
+)
+def test_array_broadcast_stops(shape_stop):
+    np_start = mk_seq_array(np, shape_start)
+    num_start = mk_seq_array(num, shape_start)
+
+    np_stop = mk_seq_array(np, shape_stop) + 5
+    num_stop = mk_seq_array(num, shape_stop) + 5
+    x = np.linspace(np_start, np_stop, num=5)
+    y = num.linspace(num_start, num_stop, num=5)
+    assert np.array_equal(x, y)
+
+
+def test_arrays_both_start_and_stop_broadcast():
+    shape_start = (1, 3)
+    np_start = mk_seq_array(np, shape_start)
+    num_start = mk_seq_array(num, shape_start)
+    shape_stop = (2, 1)
+    np_stop = mk_seq_array(np, shape_stop) + 5
+    num_stop = mk_seq_array(num, shape_stop) + 5
+
+    x = np.linspace(np_start, np_stop, num=5)
+    y = num.linspace(num_start, num_stop, num=5)
+    assert np.array_equal(x, y)
+
+
+@pytest.mark.parametrize(
+    "shape", ((0,), (3,), (2, 1)), ids=lambda shape: f"(shape={shape})"
+)
+def test_array_with_scalar(shape):
+    np_arr = mk_seq_array(np, shape)
+    num_arr = mk_seq_array(num, shape)
+    scalar = 10
+
+    x1 = np.linspace(np_arr, scalar, num=5)
+    y1 = num.linspace(num_arr, scalar, num=5)
+    assert np.array_equal(x1, y1)
+
+    x2 = np.linspace(scalar, np_arr, num=5)
+    y2 = num.linspace(scalar, num_arr, num=5)
+    assert np.array_equal(x2, y2)
+
+
+@pytest.mark.parametrize(
+    "endpoint", (True, False), ids=lambda endpoint: f"(endpoint={endpoint})"
+)
+@pytest.mark.parametrize(
+    "shape", ((0,), (2, 1)), ids=lambda shape: f"(shape={shape})"
+)
+def test_empty_array(shape, endpoint):
+    np_arr = mk_seq_array(np, shape)
+    num_arr = mk_seq_array(num, shape)
+
+    x1 = np.linspace(np_arr, [], num=5, endpoint=endpoint)
+    y1 = num.linspace(num_arr, [], num=5, endpoint=endpoint)
+    assert np.array_equal(x1, y1)
+
+    x2 = np.linspace([], np_arr, num=5, endpoint=endpoint)
+    y2 = num.linspace([], num_arr, num=5, endpoint=endpoint)
+    assert np.array_equal(x2, y2)
+
+
+@pytest.mark.parametrize(
+    "endpoint", (True, False), ids=lambda endpoint: f"(endpoint={endpoint})"
+)
+@pytest.mark.parametrize(
+    "shape", ((0,), (2, 1)), ids=lambda shape: f"(shape={shape})"
+)
+def test_empty_array_retstep(shape, endpoint):
+    np_arr = mk_seq_array(np, shape)
+    num_arr = mk_seq_array(num, shape)
+
+    x1 = np.linspace(np_arr, [], num=5, endpoint=endpoint, retstep=True)
+    y1 = num.linspace(num_arr, [], num=5, endpoint=endpoint, retstep=True)
+    assert np.array_equal(x1[0], y1[0])
+    assert np.array_equal(x1[1], y1[1])
+
+    x2 = np.linspace([], np_arr, num=5, endpoint=endpoint, retstep=True)
+    y2 = num.linspace([], num_arr, num=5, endpoint=endpoint, retstep=True)
+    assert np.array_equal(x2[0], y2[0])
+    assert np.array_equal(x2[1], y2[1])
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    "number", (0, 1, 10), ids=lambda number: f"(num={number})"
+)
+@pytest.mark.parametrize(
+    "axis", range(-3, 3), ids=lambda axis: f"(axis={axis})"
+)
+def test_arrays_axis(axis, number):
+    # In cuNumeric, if axis < -1, raise ValueError
+    # 'Point cannot exceed 4 dimensions set from LEGATE_MAX_DIM'
+    # In Numpy, if axis is -2 or -3, also pass
+    # In cuNumeric, for axis >= -1, if num=0, raise IndexError:
+    # tuple index out of range
+    # In Numpy, if num=0, pass and returns empty array
     x = np.array([[0, 1], [2, 3]])
     y = np.array([[4, 5], [6, 7]])
     xp = num.array(x)
     yp = num.array(y)
 
-    z = np.linspace(x, y, num=5, axis=0)
-    w = num.linspace(xp, yp, num=5, axis=0)
+    z = np.linspace(x, y, num=number, axis=axis)
+    w = num.linspace(xp, yp, num=number, axis=axis)
     assert np.array_equal(z, w)
 
-    z = np.linspace(x, y, num=5, axis=1)
-    w = num.linspace(xp, yp, num=5, axis=1)
-    assert np.array_equal(z, w)
 
-    z = np.linspace(x, y, num=5, axis=2)
-    w = num.linspace(xp, yp, num=5, axis=2)
-    assert np.array_equal(z, w)
+@pytest.mark.parametrize(
+    "axis", range(-1, 1), ids=lambda axis: f"(axis={axis})"
+)
+def test_scalar_axis(axis):
+    start = 2.0
+    stop = 3.0
+    x = np.linspace(start, stop, num=5, axis=axis)
+    y = num.linspace(start, stop, num=5, axis=axis)
+    assert np.array_equal(x, y)
+
+
+@pytest.mark.parametrize(
+    "dtype", (None, int, float, bool), ids=lambda dtype: f"(dtype={dtype})"
+)
+def test_dtype(dtype):
+    start = 2.0
+    stop = 3.0
+    x = np.linspace(start, stop, num=5, dtype=dtype)
+    y = num.linspace(start, stop, num=5, dtype=dtype)
+    assert np.array_equal(x, y)
+
+
+class TestLinspaceErrors:
+    def setup_method(self):
+        self.start = mk_seq_array(num, (2, 3))
+        self.stop = mk_seq_array(num, (2, 3)) + 10
+        self.num = 5
+
+    @pytest.mark.xfail
+    def test_num_float(self):
+        # In Numpy, raise TypeError
+        # In cuNumeric, pass
+        msg = "cannot be interpreted as an integer"
+        with pytest.raises(TypeError, match=msg):
+            num.linspace(0, 10, num=4.5)
+
+    def test_num_negative(self):
+        msg = "must be non-negative"
+        with pytest.raises(ValueError, match=msg):
+            num.linspace(0, 10, num=-1)
+
+    def test_num_none(self):
+        msg = "not supported between instances of 'NoneType' and 'int'"
+        with pytest.raises(TypeError, match=msg):
+            num.linspace(0, 10, num=None)
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize(
+        "axis", (-4, 3), ids=lambda axis: f"(axis={axis})"
+    )
+    def test_axis_out_of_bound_array(self, axis):
+        # In cuNumeric, if axis < -1, raise ValueError
+        # 'Point cannot exceed 4 dimensions set from LEGATE_MAX_DIM'
+        msg = "out of bounds"
+        # In Numpy, it raises AxisError
+        with pytest.raises(ValueError, match=msg):
+            num.linspace(self.start, self.stop, axis=axis)
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize(
+        "axis", (-2, 1), ids=lambda axis: f"(axis={axis})"
+    )
+    def test_axis_out_of_bound_scalar(self, axis):
+        # In cuNumeric, it pass and the result equals when axis=0
+        # In Numpy, it raises AxisError
+        msg = "out of bounds"
+        with pytest.raises(ValueError, match=msg):
+            num.linspace(2.0, 3.0, axis=axis)
+
+    def test_axis_float(self):
+        axis = 1.0
+        msg = "can't multiply sequence by non-int of type 'float'"
+        with pytest.raises(TypeError, match=msg):
+            num.linspace(self.start, self.stop, axis=axis)
+
+    @pytest.mark.xfail
+    def test_axis_none(self):
+        # In cuNumeric, pass and treat it as axis=0
+        # In Numpy, raises TypeError
+        axis = None
+        msg = "'NoneType' object is not iterable"
+        with pytest.raises(TypeError, match=msg):
+            num.linspace(self.start, self.stop, axis=axis)
+
+    @pytest.mark.parametrize(
+        "shape", ((0,), (2,), (3, 3)), ids=lambda shape: f"(shape={shape})"
+    )
+    def test_array_bad_shape(self, shape):
+        msg = "shape mismatch"
+        stop = mk_seq_array(num, shape)
+        with pytest.raises(ValueError, match=msg):
+            num.linspace(self.start, stop)
+
+    def test_start_none(self):
+        with pytest.raises(TypeError):
+            num.linspace(None, 10, num=5)
+
+    def test_stop_none(self):
+        with pytest.raises(TypeError):
+            num.linspace(0, None, num=5)
 
 
 if __name__ == "__main__":
--- tests/integration/test_swapaxes.py
+++ tests/integration/test_swapaxes.py
@@ -15,42 +15,194 @@
 
 import numpy as np
 import pytest
+from utils.generators import mk_seq_array
 
 import cunumeric as num
 
 a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
 
-def test_small():
-    a_num = num.array(a)
-    b_num = a_num.swapaxes(0, 1)
+class TestSwapAxesModule:
+    def test_small(self):
+        a_num = num.array(a)
+        b = np.swapaxes(a, 0, 1)
+        b_num = num.swapaxes(a_num, 0, 1)
+        assert np.array_equal(b, b_num)
 
-    assert num.array_equal(a_num.sum(axis=0), b_num.sum(axis=1))
+    def test_tall(self):
+        a_tall = np.concatenate((a,) * 100)
+        a_tall_num = num.array(a_tall)
+
+        b_tall = np.swapaxes(a_tall, 0, 1)
+        b_tall_num = num.swapaxes(a_tall_num, 0, 1)
+        assert np.array_equal(b_tall, b_tall_num)
+
+    def test_wide(self):
+        a_wide = np.concatenate((a,) * 100, axis=1)
+        a_wide_num = num.array(a_wide)
+
+        b_wide = np.swapaxes(a_wide, 0, 1)
+        b_wide_num = num.swapaxes(a_wide_num, 0, 1)
+        assert np.array_equal(b_wide, b_wide_num)
+
+    def test_big(self):
+        a_tall = np.concatenate((a,) * 100)
+        a_big = np.concatenate((a_tall,) * 100, axis=1)
+        a_big_num = num.array(a_big)
+
+        b_big = np.swapaxes(a_big, 0, 1)
+        b_big_num = num.swapaxes(a_big_num, 0, 1)
+        assert np.array_equal(b_big, b_big_num)
+
+    @pytest.mark.parametrize(
+        "axes",
+        ((0, 0), (-3, 1), (0, 2), (-3, -2)),
+        ids=lambda axes: f"(axes={axes})",
+    )
+    def test_axes(self, axes):
+        shape = (3, 4, 5)
+        np_arr = mk_seq_array(np, shape)
+        num_arr = num.array(np_arr)
+        axis1, axis2 = axes
+
+        res_np = np.swapaxes(np_arr, axis1, axis2)
+        res_num = num.swapaxes(num_arr, axis1, axis2)
+        assert np.array_equal(res_num, res_np)
+
+    def test_emtpy_array(self):
+        np_arr = np.array([])
+        num_arr = num.array([])
+        axis1 = 0
+        axis2 = 0
+
+        res_np = np.swapaxes(np_arr, axis1, axis2)
+        res_num = num.swapaxes(num_arr, axis1, axis2)
+        assert np.array_equal(res_num, res_np)
 
 
-def test_tall():
-    a_tall = np.concatenate((a,) * 100)
-    a_tall_num = num.array(a_tall)
-    b_tall_num = a_tall_num.swapaxes(0, 1)
+class TestSwapAxesModuleErrors:
+    def setup_method(self):
+        self.a = mk_seq_array(num, (3, 3))
 
-    assert num.array_equal(a_tall_num.sum(axis=0), b_tall_num.sum(axis=1))
+    def test_a_none(self):
+        msg = "has no attribute 'swapaxes'"
+        with pytest.raises(AttributeError, match=msg):
+            num.swapaxes(None, 0, 0)
+
+    @pytest.mark.parametrize(
+        "axes", ((3, 0), (0, 3)), ids=lambda axes: f"(axes={axes})"
+    )
+    def test_axes_out_of_bound1(self, axes):
+        axis1, axis2 = axes
+        msg = "too large for swapaxes"
+        with pytest.raises(ValueError, match=msg):
+            num.swapaxes(self.a, axis1, axis2)
+
+    @pytest.mark.parametrize(
+        "axes", ((-4, 0), (0, -4)), ids=lambda axes: f"(axes={axes})"
+    )
+    def test_axes_out_of_bound2(self, axes):
+        axis1, axis2 = axes
+        with pytest.raises(IndexError):
+            num.swapaxes(self.a, axis1, axis2)
+
+    @pytest.mark.parametrize(
+        "axes", ((None, 0), (0, None)), ids=lambda axes: f"(axes={axes})"
+    )
+    def test_axes_none(self, axes):
+        axis1, axis2 = axes
+        msg = "not supported between instances of 'NoneType' and 'int'"
+        with pytest.raises(TypeError, match=msg):
+            num.swapaxes(self.a, axis1, axis2)
 
 
-def test_wide():
-    a_wide = np.concatenate((a,) * 100, axis=1)
-    a_wide_num = num.array(a_wide)
-    b_wide_num = a_wide_num.swapaxes(0, 1)
+class TestSwapAxesArrayMethod:
+    def test_small(self):
+        a_num = num.array(a)
+        b = a.swapaxes(0, 1)
+        b_num = a_num.swapaxes(0, 1)
+        assert np.array_equal(b, b_num)
 
-    assert num.array_equal(a_wide_num.sum(axis=0), b_wide_num.sum(axis=1))
+    def test_tall(self):
+        a_tall = np.concatenate((a,) * 100)
+        a_tall_num = num.array(a_tall)
+
+        b_tall = a_tall.swapaxes(0, 1)
+        b_tall_num = a_tall_num.swapaxes(0, 1)
+        assert np.array_equal(b_tall, b_tall_num)
+
+    def test_wide(self):
+        a_wide = np.concatenate((a,) * 100, axis=1)
+        a_wide_num = num.array(a_wide)
+
+        b_wide = a_wide.swapaxes(0, 1)
+        b_wide_num = a_wide_num.swapaxes(0, 1)
+        assert np.array_equal(b_wide, b_wide_num)
+
+    def test_big(self):
+        a_tall = np.concatenate((a,) * 100)
+        a_big = np.concatenate((a_tall,) * 100, axis=1)
+        a_big_num = num.array(a_big)
+
+        b_big = a_big.swapaxes(0, 1)
+        b_big_num = a_big_num.swapaxes(0, 1)
+        assert np.array_equal(b_big, b_big_num)
+
+    @pytest.mark.parametrize(
+        "axes",
+        ((0, 0), (-3, 1), (0, 2), (-3, -2)),
+        ids=lambda axes: f"(axes={axes})",
+    )
+    def test_axes(self, axes):
+        shape = (3, 4, 5)
+        np_arr = mk_seq_array(np, shape)
+        num_arr = num.array(np_arr)
+        axis1, axis2 = axes
+
+        res_np_arr = np_arr.swapaxes(axis1, axis2)
+        res_num_arr = num_arr.swapaxes(axis1, axis2)
+        assert np.array_equal(res_num_arr, res_np_arr)
+
+    def test_emtpy_array(self):
+        np_arr = np.array([])
+        num_arr = num.array([])
+        axis1 = 0
+        axis2 = 0
+
+        res_np_arr = np_arr.swapaxes(axis1, axis2)
+        res_num_arr = num_arr.swapaxes(axis1, axis2)
+        assert np.array_equal(res_num_arr, res_np_arr)
 
 
-def test_big():
-    a_tall = np.concatenate((a,) * 100)
-    a_big = np.concatenate((a_tall,) * 100, axis=1)
-    a_big_num = num.array(a_big)
-    b_big_num = a_big_num.swapaxes(0, 1)
+class TestSwapAxesArrayMethodErrors:
+    def setup_method(self):
+        self.a = mk_seq_array(num, (3, 3))
 
-    assert num.array_equal(a_big_num.sum(axis=0), b_big_num.sum(axis=1))
+    @pytest.mark.parametrize(
+        "axes", ((3, 0), (0, 3)), ids=lambda axes: f"(axes={axes})"
+    )
+    def test_axes_out_of_bound1(self, axes):
+        axis1, axis2 = axes
+        msg = "too large for swapaxes"
+        with pytest.raises(ValueError, match=msg):
+            self.a.swapaxes(axis1, axis2)
+
+    @pytest.mark.parametrize(
+        "axes", ((-4, 0), (0, -4)), ids=lambda axes: f"(axes={axes})"
+    )
+    def test_axes_out_of_bound2(self, axes):
+        axis1, axis2 = axes
+        with pytest.raises(IndexError):
+            self.a.swapaxes(axis1, axis2)
+
+    @pytest.mark.parametrize(
+        "axes", ((None, 0), (0, None)), ids=lambda axes: f"(axes={axes})"
+    )
+    def test_axes_none(self, axes):
+        axis1, axis2 = axes
+        msg = "not supported between instances of 'NoneType' and 'int'"
+        with pytest.raises(TypeError, match=msg):
+            self.a.swapaxes(axis1, axis2)
 
 
 if __name__ == "__main__":
