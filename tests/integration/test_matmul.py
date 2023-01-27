7752ca7b00a1599b326bfd74d2874caa5112f6f9
Sai <saiy@nvidia.com>
2023-01-27 10:33:30 +0800

    Enhance test_matmul.py and test_norm.py

--- tests/integration/test_matmul.py
+++ tests/integration/test_matmul.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 #
 
+import numpy as np
 import pytest
 from legate.core import LEGATE_MAX_DIM
 from utils.contractions import (
@@ -22,6 +23,7 @@ from utils.contractions import (
     check_types,
 )
 
+import cunumeric as num
 from cunumeric.utils import matmul_modes
 
 
@@ -39,6 +41,120 @@ def test(a_ndim, b_ndim):
     check_shapes(name, modes, operation)
     if a_ndim <= 2 and b_ndim <= 2:
         check_types(name, modes, operation)
+
+
+class TestMatmulErrors:
+    @pytest.mark.parametrize(
+        "shapesAB",
+        (
+                ((2, 4), (2, 3)),
+                ((3, 2, 4), (2, 4, 3)),
+                ((3, 2, 4), (3, 2, 3)),
+        ),
+        ids=lambda shapesAB: f"(shapesAB={shapesAB})",
+    )
+    def test_invalid_shape_dim_greater_than_one(self, shapesAB):
+        shapeA, shapeB = shapesAB
+        A = num.ones(shapeA)
+        B = num.ones(shapeB)
+        with pytest.raises(ValueError):
+            num.matmul(A, B)
+
+    @pytest.mark.parametrize(
+        "shapesAB",
+        (
+                ((3, 2), (3,)),
+                pytest.param(((4, 1), (3,)), marks=pytest.mark.xfail),
+                ((1, 4), (3,)),
+                ((3,), (2, 3)),
+                ((3,), (4, 1)),
+                pytest.param(((3,), (1, 4)), marks=pytest.mark.xfail),
+                ((3,), (2,)),
+                pytest.param(((3,), (1,)), marks=pytest.mark.xfail),
+        ),
+        ids=lambda shapesAB: f"(shapesAB={shapesAB})",
+    )
+    def test_invalid_shape_with_vector(self, shapesAB):
+        # For ((4, 1), (3,)), ((3,), (1, 4)), ((3,), (1,)),
+        # In Numpy, raise ValueError
+        # In cuNumeric, broadcast 1 to 3 and pass
+        shapeA, shapeB = shapesAB
+        A = num.ones(shapeA)
+        B = num.ones(shapeB)
+        with pytest.raises(ValueError):
+            num.matmul(A, B)
+
+    def test_invalid_shape_with_scalar(self):
+        with pytest.raises(ValueError):
+            num.matmul(3, 3)
+
+        with pytest.raises(ValueError):
+            num.matmul(3, num.ones((1,)))
+        with pytest.raises(ValueError):
+            num.matmul(num.ones((1,)), 3)
+
+        with pytest.raises(ValueError):
+            num.matmul(3, num.ones((1, 1)))
+        with pytest.raises(ValueError):
+            num.matmul(num.ones((1,1)), 3)
+
+    @pytest.mark.parametrize(
+        "shape", ((2, 3), (3, 4, 3)), ids=lambda shape: f"(shape={shape})"
+    )
+    def test_out_invalid_shape(self, shape):
+        A = num.ones((3, 2, 4))
+        B = num.ones((3, 4, 3))
+        out = num.zeros(shape)
+        with pytest.raises(ValueError):
+            num.matmul(A, B, out=out)
+
+    @pytest.mark.xfail
+    def test_out_invalid_shape_DIVERGENCE(self):
+        # In Numpy, PASS
+        # In cuNumeric, raise ValueError
+        A = num.ones((3, 2, 4))
+        B = num.ones((3, 4, 3))
+        shape = (3, 3, 2, 3)
+        out = num.zeros(shape)
+        num.matmul(A, B, out=out)
+
+
+    def test_out_invalid_dtype(self):
+        A = num.ones((3, 2, 4))
+        B = num.ones((3, 4, 3))
+        dtype = np.int64
+        out = num.zeros((3, 2, 3), dtype=dtype)
+        with pytest.raises(TypeError):
+            num.matmul(A, B, out=out)
+
+    @pytest.mark.parametrize(
+        "casting_dtype",
+        (
+                ('no', np.float32),
+                ('equiv', np.float32),
+                ('safe', np.float32),
+                ('same_kind', np.int64),
+        ),
+        ids=lambda casting_dtype: f"(casting_dtype={casting_dtype})",
+    )
+    def test_invalid_casting_dtype(self, casting_dtype):
+        # In Nmupy, it raises numpy.core._exceptions.UFuncTypeError
+        # In cuNumeric, it raises TypeError
+        casting, dtype = casting_dtype
+        A = num.ones((2, 4))
+        B = num.ones((4, 3))
+        with pytest.raises(TypeError):
+            num.matmul(A, B, casting=casting, dtype=dtype)
+
+    @pytest.mark.xfail
+    def test_invalid_casting(self):
+        # In Numpy, raise ValueError
+        # In cuNumeric, pass
+        casting = "unknown"
+        A = num.ones((2, 4))
+        B = num.ones((4, 3))
+        with pytest.raises(ValueError):
+            num.matmul(A, B, casting=casting)
 
 
 if __name__ == "__main__":
