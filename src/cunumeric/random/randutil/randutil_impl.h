b4208870a6e8a938deac908fa50163f6b8f3e97c
Manolis Papadakis <mpapadakis@nvidia.com>
2023-02-20 13:38:21 -0800

    Use CUDA error check macros from core

--- src/cunumeric/random/randutil/randutil_impl.h
+++ src/cunumeric/random/randutil/randutil_impl.h
@@ -17,37 +17,6 @@
 #pragma once
 #include "randutil.h"
 
-#define CURAND_CHECK_LINE(a, file, line)                          \
-  {                                                               \
-    curandStatus_t __curer = a;                                   \
-    if (CURAND_STATUS_SUCCESS != __curer) { throw(int) __curer; } \
-  }
-#define CURAND_CHECK(a) CURAND_CHECK_LINE(a, __FILE__, __LINE__)
-
-#define CU_CHECK_LINE(a, file, line)                \
-  {                                                 \
-    CUresult __cuer = a;                            \
-    if (CUDA_SUCCESS != __cuer) {                   \
-      if (__cuer == CUDA_ERROR_OUT_OF_MEMORY)       \
-        throw(int) CURAND_STATUS_ALLOCATION_FAILED; \
-      else                                          \
-        throw(int) CURAND_STATUS_INTERNAL_ERROR;    \
-    }                                               \
-  }
-#define CU_CHECK(a) CU_CHECK_LINE(a, __FILE__, __LINE__)
-
-#define CUDA_CHECK_LINE(a, file, line)              \
-  {                                                 \
-    cudaError_t __cuer = a;                         \
-    if (cudaSuccess != __cuer) {                    \
-      if (__cuer == cudaErrorMemoryAllocation)      \
-        throw(int) CURAND_STATUS_ALLOCATION_FAILED; \
-      else                                          \
-        throw(int) CURAND_STATUS_INTERNAL_ERROR;    \
-    }                                               \
-  }
-#define CUDA_CHECK(a) CUDA_CHECK_LINE(a, __FILE__, __LINE__)
-
 namespace randutilimpl {
 
 enum class execlocation : int { DEVICE = 0, HOST = 1 };
