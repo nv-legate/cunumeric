1b04b8dbfc1655f1e1c58605450b03c16e05df36
Wonchan Lee <wonchanl@nvidia.com>
2023-02-23 15:11:42 -0800

    Fetch machine info from the core runtime

--- cunumeric/runtime.py
+++ cunumeric/runtime.py
@@ -70,18 +70,6 @@ class Runtime(object):
                 ty.int32,
             )
         )
-        self.num_procs = int(
-            self.legate_context.get_tunable(
-                CuNumericTunable.NUM_PROCS,
-                ty.int32,
-            )
-        )
-        self.num_gpus = int(
-            self.legate_context.get_tunable(
-                CuNumericTunable.NUM_GPUS,
-                ty.int32,
-            )
-        )
 
         # Make sure that our CuNumericLib object knows about us so it can
         # destroy us
@@ -94,6 +82,14 @@ class Runtime(object):
 
         if self.num_gpus > 0 and settings.preload_cudalibs():
             self._load_cudalibs()
+
+    @property
+    def num_procs(self) -> int:
+        return len(self.legate_runtime.machine)
+
+    @property
+    def num_gpus(self) -> int:
+        return self.legate_runtime.num_gpus
 
     def _register_dtypes(self) -> None:
         type_system = self.legate_context.type_system
