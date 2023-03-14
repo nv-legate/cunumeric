ce3abd649625fe02785742639ef4c27a6dce1bda
Bryan Van de Ven <bvandeven@nvidia.com>
2023-03-14 09:50:23 -0700

    Use explicit task creation APIs (#842)

--- cunumeric/runtime.py
+++ cunumeric/runtime.py
@@ -117,18 +117,16 @@ class Runtime(object):
         self.api_calls.append((name, location, implemented))
 
     def _load_cudalibs(self) -> None:
-        task = self.legate_context.create_task(
+        task = self.legate_context.create_manual_task(
             CuNumericOpCode.LOAD_CUDALIBS,
-            manual=True,
             launch_domain=Rect(lo=(0,), hi=(self.num_gpus,)),
         )
         task.execute()
         self.legate_runtime.issue_execution_fence(block=True)
 
     def _unload_cudalibs(self) -> None:
-        task = self.legate_context.create_task(
+        task = self.legate_context.create_manual_task(
             CuNumericOpCode.UNLOAD_CUDALIBS,
-            manual=True,
             launch_domain=Rect(lo=(0,), hi=(self.num_gpus,)),
         )
         task.execute()
@@ -224,9 +222,8 @@ class Runtime(object):
     ) -> int:
         self.current_random_bitgenid = self.current_random_bitgenid + 1
         if forceCreate:
-            task = self.legate_context.create_task(
+            task = self.legate_context.create_manual_task(
                 CuNumericOpCode.BITGENERATOR,
-                manual=True,
                 launch_domain=Rect(lo=(0,), hi=(self.num_procs,)),
             )
             self.bitgenerator_populate_task(
@@ -254,9 +251,8 @@ class Runtime(object):
         else:
             # with explicit destruction, do schedule a task
             self.legate_runtime.issue_execution_fence()
-            task = self.legate_context.create_task(
+            task = self.legate_context.create_manual_task(
                 CuNumericOpCode.BITGENERATOR,
-                manual=True,
                 launch_domain=Rect(lo=(0,), hi=(self.num_procs,)),
             )
             self.bitgenerator_populate_task(
