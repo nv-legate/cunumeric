9a486ee4ec5614ced48549e5b13ac09cea7e8be7
Robin Wang <104830875+robinwnv@users.noreply.github.com>
2023-05-05 09:10:56 +0800

    Enhance test_logic.py and test_logical.py (#911)
    * Enhance test_logic.py and test_logical.py
    
    * Address comments.
    
    * Address comments - part 2

--- tests/integration/test_logic.py
+++ tests/integration/test_logic.py
@@ -20,57 +20,143 @@ import pytest
 
 import cunumeric as num
 
-
-def test_inf():
-    arr = [np.inf, -np.inf, np.nan, 0]
-    assert np.array_equal(np.isposinf(arr), num.isposinf(arr))
-    assert np.array_equal(np.isneginf(arr), num.isneginf(arr))
+SCALARS_INF = (np.inf, -np.inf, np.nan, 0)
+ARRAYS_INF = ([np.inf, -np.inf, np.nan, 0],)
 
 
-INPUTS = (
+@pytest.mark.parametrize("x", SCALARS_INF + ARRAYS_INF)
+@pytest.mark.parametrize("func_name", ("isposinf", "isneginf"))
+def test_inf_basic(func_name, x):
+    func_np = getattr(np, func_name)
+    func_num = getattr(num, func_name)
+
+    assert np.array_equal(func_np(x), func_num(x))
+
+
+@pytest.mark.parametrize("out_dt", (bool, int, float))
+@pytest.mark.parametrize("x", (np.inf,) + ARRAYS_INF)
+@pytest.mark.parametrize("func_name", ("isposinf", "isneginf"))
+def test_inf_out(func_name, x, out_dt):
+    res_shape = (4,)
+    res_np = np.empty(res_shape, dtype=out_dt)
+    res_num = num.empty(res_shape, dtype=out_dt)
+
+    func_np = getattr(np, func_name)
+    func_num = getattr(num, func_name)
+
+    func_np(x, out=res_np)
+    func_num(x, out=res_num)
+    assert np.array_equal(res_np, res_num)
+
+
+class TestInfErrors:
+    @pytest.mark.parametrize("func_name", ("isposinf", "isneginf"))
+    def test_out_invalid_shape(self, func_name):
+        expected_exc = ValueError
+        x = [np.inf, -np.inf, np.nan, 0]
+        res_shape = (3,)
+        res_np = np.empty(res_shape)
+        res_num = num.empty(res_shape)
+
+        func_np = getattr(np, func_name)
+        func_num = getattr(num, func_name)
+
+        with pytest.raises(expected_exc):
+            func_np(x, out=res_np)
+        with pytest.raises(expected_exc):
+            func_num(x, out=res_num)
+
+
+SCALARS = (pytest.param("a string", marks=pytest.mark.xfail), None, False)
+ARRAYS = (
     [1.0, 2.0, 3.0],
     [1.0 + 0j, 2.0 + 0j, 3.0 + 0j],
     [1.0 + 1j, 2.0 + 0j, 3.0 + 1j],
 )
 
 
-@pytest.mark.parametrize("arr", INPUTS)
-def test_predicates(arr):
-    in_np = np.array(arr)
-    in_num = num.array(arr)
-
-    assert np.array_equal(np.isreal(in_np), num.isreal(in_num))
-    assert np.array_equal(np.iscomplex(in_np), num.iscomplex(in_num))
+@pytest.mark.parametrize("x", SCALARS + ARRAYS)
+def test_isreal(x):
+    # for x is 'a string', np.isreal is False, num.isreal is Array(True)
+    assert np.array_equal(np.isreal(x), num.isreal(x))
 
 
-@pytest.mark.parametrize("arr", INPUTS)
-def test_array_predicates(arr):
-    in_np = np.array(arr)
-    in_num = num.array(arr)
+@pytest.mark.parametrize("x", SCALARS + ARRAYS)
+def test_iscomplex(x):
+    assert np.array_equal(np.iscomplex(x), num.iscomplex(x))
+
+
+@pytest.mark.parametrize("x", SCALARS)
+def test_isrealobj_scalar(x):
+    assert np.array_equal(np.isrealobj(x), num.isrealobj(x))
+
+
+@pytest.mark.parametrize("x", SCALARS)
+def test_iscomplexobj_scalar(x):
+    assert np.array_equal(np.iscomplexobj(x), num.iscomplexobj(x))
+
+
+@pytest.mark.parametrize("x", ARRAYS)
+def test_isrealobj_array(x):
+    in_np = np.array(x)
+    in_num = num.array(x)
 
     assert np.array_equal(np.isrealobj(in_np), num.isrealobj(in_num))
     assert np.array_equal(np.isrealobj(in_np), num.isrealobj(in_np))
+
+
+@pytest.mark.parametrize("x", ARRAYS)
+def test_iscomplexobj_array(x):
+    in_np = np.array(x)
+    in_num = num.array(x)
+
     assert np.array_equal(np.iscomplexobj(in_np), num.iscomplexobj(in_num))
     assert np.array_equal(np.iscomplexobj(in_np), num.iscomplexobj(in_np))
 
 
-def test_isscalar():
+@pytest.mark.parametrize("x", (1.0, True, [1, 2, 3]))
+def test_isscalar(x):
+    assert np.isscalar(x) is num.isscalar(x)
+
+
+def test_isscalar_array():
     in_np = np.array([1, 2, 3])
     in_num = num.array([1, 2, 3])
-
-    assert num.isscalar(1.0)
-    assert num.isscalar(True)
-    assert not num.isscalar(in_np)
-    assert not num.isscalar(in_num)
+    assert np.isscalar(in_np) is num.isscalar(in_num) is False
 
     # NumPy's scalar reduction returns a Python scalar
-    assert num.isscalar(np.sum(in_np))
+    assert num.isscalar(np.sum(in_np)) is True
     # but cuNumeric's scalar reduction returns a 0-D array that behaves like
     # a deferred scalar
-    assert not num.isscalar(num.sum(in_np))
+    assert num.isscalar(num.sum(in_np)) is False
 
 
-def test_isclose():
+SCALAR_PAIRS = (
+    (0, -1e-8),
+    (1e10, 1.00001e10),
+    (1 + 1j, 1 + 1.00001j),
+    (0, -0.000001),
+    (1e10, 1.0001e10),
+    (1 + 1j, 1 + 1.0001j),
+)
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    ("a", "b"),
+    SCALAR_PAIRS,
+)
+def test_isclose_scalars(a, b):
+    # for all cases,
+    # In Numpy, it pass
+    # In cuNumeric, it raises IndexError: too many indices for array:
+    # array is 0-dimensional, but 1 were indexed
+    out_np = np.isclose(a, b)
+    out_num = num.isclose(a, b)
+    assert np.array_equal(out_np, out_num)
+
+
+def test_isclose_arrays():
     in1_np = np.random.rand(10)
     in2_np = in1_np + np.random.uniform(low=5e-09, high=2e-08, size=10)
     in1_num = num.array(in1_np)
@@ -80,16 +166,110 @@ def test_isclose():
     out_num = num.isclose(in1_num, in2_num)
     assert np.array_equal(out_np, out_num)
 
-    weird_values = [np.inf, -np.inf, np.nan, 0.0, -0.0]
-    weird_pairs = tuple(combinations_with_replacement(weird_values, 2))
-    in1_np = np.array([x for x, _ in weird_pairs])
-    in2_np = np.array([y for _, y in weird_pairs])
+
+SHAPES_BROADCASTING = (
+    (1, 3),
+    (2, 3),
+    (1, 2, 3),
+    (2, 2, 3),
+    (1,),
+    (1, 1),
+    (1, 2, 1),
+    (2, 2, 1),
+)
+
+
+@pytest.mark.parametrize(
+    "shape_b", SHAPES_BROADCASTING, ids=lambda shape_b: f"(shape_b={shape_b})"
+)
+def test_isclose_broadcast(shape_b):
+    len_in_arr = 20
+    in1_np = np.random.rand(len_in_arr)
+    in2_np = in1_np + np.random.uniform(low=5e-09, high=2e-08, size=len_in_arr)
+
+    shape_a = (3,)
+    size_a = np.prod(shape_a)
+    a_np = np.array([in1_np[i % len_in_arr] for i in range(size_a)]).reshape(
+        shape_a
+    )
+
+    size_b = np.prod(shape_b)
+    b_np = np.array([in2_np[i % len_in_arr] for i in range(size_b)]).reshape(
+        shape_b
+    )
+
+    a_num = num.array(a_np)
+    b_num = num.array(b_np)
+    out_np = np.isclose(a_np, b_np)
+    out_num = num.isclose(a_num, b_num)
+    assert np.array_equal(out_np, out_num)
+
+
+EMPTY_ARRAY_PAIRS = (
+    ([], []),
+    ([], [[]]),
+    ([[]], [[]]),
+)
+
+
+@pytest.mark.parametrize(
+    ("a", "b"),
+    EMPTY_ARRAY_PAIRS,
+)
+def test_isclose_empty_arrays(a, b):
+    out_np = np.isclose(a, b)
+    out_num = num.isclose(a, b)
+    assert np.array_equal(out_np, out_num)
+
+
+@pytest.mark.parametrize(
+    ("rtol", "atol"),
+    ((1e-04, 1e-06), (1e-06, 1e-09)),
+)
+def test_isclose_arrays_rtol_atol(rtol, atol):
+    in1_np = np.random.rand(10)
+    in2_np = in1_np + np.random.uniform(low=5e-09, high=2e-08, size=10)
     in1_num = num.array(in1_np)
     in2_num = num.array(in2_np)
 
-    out_np = np.isclose(in1_np, in2_np)
-    out_num = num.isclose(in1_num, in2_num)
+    out_np = np.isclose(in1_np, in2_np, rtol=rtol, atol=atol)
+    out_num = num.isclose(in1_num, in2_num, rtol=rtol, atol=atol)
     assert np.array_equal(out_np, out_num)
+
+
+@pytest.mark.parametrize(
+    "equal_nan", (False, pytest.param(True, marks=pytest.mark.xfail))
+)
+def test_isclose_euqal_nan(equal_nan):
+    # If equal_nan is True,
+    # In Numpy, it pass
+    # In cuNumeric, it raises NotImplementedError
+    values = [np.inf, -np.inf, np.nan, 0.0, -0.0]
+    pairs = tuple(combinations_with_replacement(values, 2))
+    in1_np = np.array([x for x, _ in pairs])
+    in2_np = np.array([y for _, y in pairs])
+    in1_num = num.array(in1_np)
+    in2_num = num.array(in2_np)
+
+    out_np = np.isclose(in1_np, in2_np, equal_nan=equal_nan)
+    out_num = num.isclose(in1_num, in2_num, equal_nan=equal_nan)
+    assert np.array_equal(out_np, out_num)
+
+
+class TestIsCloseErrors:
+    def test_arrays_invalid_shape(self):
+        expected_exc = ValueError
+        a_np = np.random.rand(6)
+        b_np = a_np + np.random.uniform(low=5e-09, high=2e-08, size=6)
+        in1_np = a_np.reshape((2, 3))
+        in2_np = b_np.reshape((3, 2))
+        in1_num = num.array(in1_np)
+        in2_num = num.array(in2_np)
+
+        with pytest.raises(expected_exc):
+            np.isclose(in1_np, in2_np)
+        with pytest.raises(expected_exc):
+            num.isclose(in1_num, in2_num)
 
 
 if __name__ == "__main__":
