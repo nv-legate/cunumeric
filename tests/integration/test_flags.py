b24d23e9faf69aad5266d2f9aee52a02ff2d9b24
Bryan Van de Ven <bvandeven@nvidia.com>
2023-05-19 09:39:24 -0700

    drastically simplify flags tests

--- tests/integration/test_flags.py
+++ tests/integration/test_flags.py
@@ -20,131 +20,49 @@ import cunumeric as num
 
 DIM_CASE = (5, 5)
 
+FLAGS = (
+    "c_contiguous",
+    "f_contiguous",
+    "owndata",
+    "writeable",
+    "aligned",
+    "writebackifcopy",
+    "behaved",
+    "carray",
+    "farray",
+)
 
-def _print_result(test_result, print_msg, err_arr):
-    assert test_result, (
-        f"Failed, {print_msg}\n"
-        f"Attr, {err_arr[0]}\n"
-        f"numpy result: {err_arr[1]}\n"
-        f"cunumeric_result: {err_arr[2]}\n"
-        f"cunumeric and numpy shows"
-        f" different result\n"
-    )
-    print(f"Passed, {print_msg}")
+SHORT_FLAGS = ("C", "F", "O", "W", "A", "X", "B", "CA", "FA")
 
 
 class Test_flags:
-    attrs = {
-        "C": "c_contiguous",
-        "F": "f_contiguous",
-        "O": "owndata",
-        "W": "writeable",
-        "A": "aligned",
-        "X": "writebackifcopy",
-        "B": "behaved",
-        "CA": "carray",
-        "FA": "farray",
-    }
+    @pytest.mark.parametrize("flag", FLAGS)
+    def test_default_attr(self, flag):
+        npflags = np.zeros(shape=DIM_CASE).flags
+        numflags = num.zeros(shape=DIM_CASE).flags
 
-    @pytest.mark.parametrize("view_test", [False, True], ids=bool)
-    def test_default_flags_attr(self, view_test):
-        arr_np = np.zeros(shape=DIM_CASE)
-        arr_num = num.zeros(shape=DIM_CASE)
-        if view_test:
-            arr_np = arr_np.view()
-            arr_num = arr_num.view()
-        # set 'false' to `aligned`
-        # because the default value for cunumeric is false
-        arr_np.flags.aligned = False
-        b = arr_np.flags
-        c = arr_num.flags
-        is_equal = True
-        err_arr = None
-        arr_np.flags.aligned = False
-        # test default values in `ndarray.flags`
-        for _, attr in self.attrs.items():
-            if getattr(b, attr) != getattr(c, attr):
-                is_equal = False
-                err_arr = [attr, getattr(b, attr), getattr(c, attr)]
-                break
+        assert getattr(npflags, flag) == getattr(numflags, flag)
 
-        _print_result(is_equal, "np.ndarray.flags", err_arr)
+    @pytest.mark.parametrize("flag", FLAGS)
+    def test_default_attr_view(self, flag):
+        npflags = np.zeros(shape=DIM_CASE).view().flags
+        numflags = num.zeros(shape=DIM_CASE).view().flags
 
-    @pytest.mark.parametrize("view_test", [False, True], ids=bool)
-    def test_default_flags_element(self, view_test):
-        arr_np = np.zeros(shape=DIM_CASE)
-        arr_num = num.zeros(shape=DIM_CASE)
-        if view_test:
-            arr_np = arr_np.view()
-            arr_num = arr_num.view()
-        # set 'false' to `aligned`
-        # because the default value for cunumeric is false
-        arr_np.flags.aligned = False
-        b = arr_np.flags
-        c = arr_num.flags
-        is_equal = True
-        err_arr = None
-        short = ["C", "F", "O", "W", "X", "B", "FA", "CA"]
-        # test default values in `ndarrray.flags`
-        for attr in self.attrs:
-            attr_upper = attr.upper()
-            if b[attr_upper] != c[attr_upper]:
-                is_equal = False
-                err_arr = [attr, b[attr_upper], c[attr_upper]]
-                break
+        assert getattr(npflags, flag) == getattr(numflags, flag)
 
-        for idx, attr in enumerate(short):
-            if b[attr] != c[attr]:
-                is_equal = False
-                err_arr = [attr, b[attr], c[attr]]
-                break
-            if b[attr] is not b[list(self.attrs)[idx].upper()]:
-                is_equal = False
-                err_arr = [
-                    (attr, list(self.attrs)[idx].upper()),
-                    b[attr],
-                    list(self.attrs)[idx].upper(),
-                ]
-                break
+    @pytest.mark.parametrize("flag", SHORT_FLAGS)
+    def test_default_item(self, flag):
+        npflags = np.zeros(shape=DIM_CASE).flags
+        numflags = num.zeros(shape=DIM_CASE).flags
 
-        _print_result(is_equal, "np.ndarray.flags", err_arr)
+        assert npflags[flag] == numflags[flag]
 
-    @pytest.mark.parametrize("view_test", [False, True], ids=bool)
-    def test_setflags(self, view_test):
-        arr_np = np.zeros(shape=DIM_CASE)
-        arr_num = num.zeros(shape=DIM_CASE)
-        if view_test:
-            arr_np = arr_np.view()
-            arr_num = arr_num.view()
-        arr_np.flags.aligned = False
-        b = arr_np
-        c = arr_num
-        is_equal = True
-        err_arr = None
-        # we don't test `aligned`
-        # because the setter for `aligned` is not implemented
-        for short, attr in list(self.attrs.items())[3:5]:
-            if attr == "aligned":
-                continue
-            # alter flags
-            error_b = False
-            error_c = False
-            try:
-                b.flags[short] = not b.flags[short]
-            except ValueError:
-                error_b = True
-            try:
-                c.flags[short] = not c.flags[short]
-            except ValueError:
-                error_c = True
-            if attr == "writebackifcopy":
-                is_equal = error_b and error_c
-            else:
-                is_equal = b.flags[short] == c.flags[short]
-            err_arr = [("flags", attr), b.flags[short], c.flags[short]]
-            if not is_equal:
-                break
-        _print_result(is_equal, "np.ndarray.flags", err_arr)
+    @pytest.mark.parametrize("flag", SHORT_FLAGS)
+    def test_default_item_view(self, flag):
+        npflags = np.zeros(shape=DIM_CASE).view().flags
+        numflags = num.zeros(shape=DIM_CASE).view().flags
+
+        assert npflags[flag] == numflags[flag]
 
 
 if __name__ == "__main__":
