58e3be34ace95a2c613652c18f184151a2f982ae
Andrei Schaffer <aschaffer@nvidia.com>
2023-07-14 18:43:52 -0500

    Removed some dead code.

--- src/cunumeric/stat/histogram_omp.cc
+++ src/cunumeric/stat/histogram_omp.cc
@@ -33,61 +33,6 @@
 namespace cunumeric {
 using namespace legate;
 
-namespace detail {
-
-// RO accessor (size, pointer) extractor:
-//
-template <typename VAL>
-std::tuple<size_t, const VAL*> get_accessor_ptr(const AccessorRO<VAL, 1>& src_acc,
-                                                const Rect<1>& src_rect)
-{
-  size_t src_strides[1];
-  const VAL* src_ptr = src_acc.ptr(src_rect, src_strides);
-  assert(src_strides[0] == 1);
-  //
-  // const VAL* src_ptr: need to create a copy with create_buffer(...);
-  // since src will get sorted (in-place);
-  //
-  size_t src_size = src_rect.hi - src_rect.lo + 1;
-  return std::make_tuple(src_size, src_ptr);
-}
-// RD accessor (size, pointer) extractor:
-//
-template <typename VAL>
-std::tuple<size_t, VAL*> get_accessor_ptr(const AccessorRD<SumReduction<VAL>, true, 1>& src_acc,
-                                          const Rect<1>& src_rect)
-{
-  size_t src_strides[1];
-  VAL* src_ptr = src_acc.ptr(src_rect, src_strides);
-  assert(src_strides[0] == 1);
-  //
-  // const VAL* src_ptr: need to create a copy with create_buffer(...);
-  // since src will get sorted (in-place);
-  //
-  size_t src_size = src_rect.hi - src_rect.lo + 1;
-  return std::make_tuple(src_size, src_ptr);
-}
-#ifdef _USE_THRUST_
-// accessor copy utility:
-//
-template <typename VAL>
-std::tuple<size_t, Buffer<VAL>, const VAL*> make_accessor_copy(const AccessorRO<VAL, 1>& src_acc,
-                                                               const Rect<1>& src_rect)
-{
-  size_t src_strides[1];
-  const VAL* src_ptr = src_acc.ptr(src_rect, src_strides);
-  assert(src_strides[0] == 1);
-  //
-  // const VAL* src_ptr: need to create a copy with create_buffer(...);
-  // since src will get sorted (in-place);
-  //
-  size_t src_size      = src_rect.hi - src_rect.lo + 1;
-  Buffer<VAL> src_copy = create_buffer<VAL>(src_size);
-  return std::make_tuple(src_size, src_copy, src_ptr);
-}
-#endif
-}  // namespace detail
-
 template <Type::Code CODE>
 struct HistogramImplBody<VariantKind::OMP, CODE> {
   using VAL = legate_type_of<CODE>;
