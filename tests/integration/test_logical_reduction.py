a7ad064043f04161c48786f9ba77a5177977478e
Joseph Thomas Guman <joeytg@sapling2.stanford.edu>
2024-02-07 15:47:33 -0800

    Minor style change to test for logical_and.reduce() \n\nSigned-off-by: Joseph Guman <joeytg@stanford.edu>

--- tests/integration/test_logical_reduction.py
+++ tests/integration/test_logical_reduction.py
@@ -4,17 +4,20 @@ from utils.comparisons import allclose
 
 import cunumeric as num
 
+
 def test_logical_and_reduce():
-    input = [[[12,0,1,2],[9,0,0,1]],[[0,0,0,5],[1,1,1,1]]]
+    input = [[[12, 0, 1, 2], [9, 0, 0, 1]], [[0, 0, 0, 5], [1, 1, 1, 1]]]
     in_num = num.array(input)
     in_np = np.array(input)
 
-    axes = [None, 0, 1, 2, (0,1,2)]
+    axes = [None, 0, 1, 2, (0, 1, 2)]
     for axis in axes:
         out_num = num.logical_and.reduce(in_num, axis=axis)
         out_np = np.logical_and.reduce(in_np, axis=axis)
-        assert(allclose(out_num, out_np, check_dtype=True))
+        assert allclose(out_num, out_np, check_dtype=True)
+
 
 if __name__ == "__main__":
     import sys
+
     sys.exit(pytest.main(sys.argv))
